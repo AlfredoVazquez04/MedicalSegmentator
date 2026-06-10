3d942c36f1ac04ff5019ad4f1eff8465a8603026
Alfredo Vázquez Rodríguez <alfredo.vazquez@rai.usc.es>
2026-06-10 15:36:46 +0200

    fixed error git unet

--- src/models/utils.py
+++ src/models/utils.py
@@ -10,7 +10,17 @@ def get_model(args):
         if args.model == 'unet':
             from .dim3 import UNet
             return UNet(
-
+                spatial_dims=args.spatial_dims,
+                in_channels=args.in_channels,
+                out_channels=args.out_channels,
+                channels=args.channels,
+                strides=args.strides,
+                kernel_size=args.kernel_size,
+                up_kernel_size=args.up_kernel_size,
+                num_res_units=args.num_res_units,
+                dropout=args.dropout,
+                bias=args.bias,
+                adn_ordering=args.adn_ordering
             )
         
     else:
